df6878ef39f06c577b05d4dd74b42624818b94e8
Kevin-Aaaquil <eshansingh2409@gmail.com>
2023-04-14 15:50:54 +0530

    get issues+pr

--- src/repo/repo-controller.ts
+++ src/repo/repo-controller.ts
@@ -1,7 +1,7 @@
 // all routes with /repo prefix
 import { Router, Request, Response, NextFunction } from "express";
 import { octokit } from "..";
-import { getRepoCommits } from "./repo-service";
+import { getRepoCommits, getRepoIssues } from "./repo-service";
 
 const handleGetRepoCommits = async (
   req: Request,
@@ -10,15 +10,32 @@ const handleGetRepoCommits = async (
 ) => {
   try {
     const data = await getRepoCommits(String(req.query.owner), String(req.query.repo), +req.query.page || 1);
-    res.status(200).send({ data });
+    res.status(200).send(data);
   } catch (err) {
-    res.status(404).send("INVALID DETAILS");
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+};
+
+const handleGetRepoIssues = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try{
+    const data = await getRepoIssues(String(req.query.owner), String(req.query.repo),+req.query.page || 1, String(req.query.state));
+    res.status(200).send(data);
+  }
+  catch(err) {
+    console.log(err);
+    res.status(500).send(err.message);
   }
 };
 
 const repoController = () => {
   const router = Router();
   router.get("/commits", handleGetRepoCommits);
+  router.get("/issues", handleGetRepoIssues);
   return router;
 };
 export default repoController;
--- src/repo/repo-service.ts
+++ src/repo/repo-service.ts
@@ -5,34 +5,87 @@ export const getRepoCommits = async (
   repo: string,
   page: number
 ) => {
-    console.log(owner, repo, page)
-  const data = await octokit.request("GET /repos/{owner}/{repo}/commits", {
-    owner: owner,
-    repo: repo,
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-    per_page: 100,
-    page: page,
-  });
-  return {
-    totalNoOfCommits: await getTotalCommits(owner, repo),
-    pageNum: page,
-    commitsOnThisPage: data.data.length,
-    lastPage: Math.ceil(await getTotalCommits(owner, repo) / 100),
-    data: data.data,
-  };
+  try {
+    console.log(owner, repo, page);
+    const data = await octokit.request("GET /repos/{owner}/{repo}/commits", {
+      owner: owner,
+      repo: repo,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+      per_page: 100,
+      page: page,
+    });
+    return {
+      totalNoOfCommits: await getTotalCommits(owner, repo),
+      pageNum: page,
+      commitsOnThisPage: data.data.length,
+      lastPage: Math.ceil((await getTotalCommits(owner, repo)) / 100),
+      data: data.data,
+    };
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const getTotalCommits = async (owner: string, repo: string) => {
-  const data = await octokit.request("GET /repos/{owner}/{repo}/commits", {
-    owner: owner,
-    repo: repo,
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-    per_page: 1,
-  });
-  const tmp = data.headers.link.split("page=").slice(-1).toString().split(">");
-  return parseInt(tmp[0]);
+  try {
+    const data = await octokit.request("GET /repos/{owner}/{repo}/commits", {
+      owner: owner,
+      repo: repo,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+      per_page: 1,
+    });
+    const tmp = data.headers.link
+      .split("page=")
+      .slice(-1)
+      .toString()
+      .split(">");
+    return parseInt(tmp[0]);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getRepoIssues = async (
+  owner: string,
+  repo: string,
+  page: number,
+  state: string,
+) => {
+  try {
+    console.log("HELLO")
+    if (state === "undefined") 
+    {
+      state = "open"
+    };
+    console.log(owner, repo, page, state);
+    let STATE: "open" | "closed" | "all";
+    if (!(state === "open" || state === "closed" || state === "all")) 
+      throw new Error("Invalid state");
+      STATE = state;
+    
+    const data = await octokit.request(
+      "GET /repos/{owner}/{repo}/issues",
+      {
+        owner: owner,
+        repo: repo,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+        per_page: 100,
+        page: page,
+        state: state,
+      }
+    );
+    console.log(data.headers);
+    return {
+      total: data.data.length,
+      data: data.data,
+    };
+  } catch (error) {
+    throw error;
+  }
 };
