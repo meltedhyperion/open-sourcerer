b67062ebc5c917232cd5df11de7cce2e9df6dfc5
Kevin-Aaaquil <eshansingh2409@gmail.com>
2023-04-14 13:54:34 +0530

    get repo commits

--- src/configs.ts
+++ src/configs.ts
@@ -6,4 +6,5 @@ export default {
     PORT: process.env.PORT || 3000,
     MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce',
     REDIS_URI: process.env.REDIS_URI || 'redis://localhost:6379',
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
-import express from 'express';
-import configs from './configs';
+import express from "express";
+import { Octokit } from "octokit";
+import configs from "./configs";
 
+(async () => {
+  const app = express();
 
-(async ()=>{
-    const app = express();
-    
-    app.get('/', (req, res)=>{
-        res.send('Hello World');
-    })
+  const octokit = new Octokit({
+    auth: configs.GITHUB_TOKEN,
+  });
 
-    const port = configs.PORT
-    app.listen(port, ()=>{
-        console.log('Server is running on port 3000');
-    })
-})()
+  app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+
+  // get repository commits
+  app.get("/repo/commits/:owner/:repo", async (req, res) => {
+    try {
+      // console.log(req.params);
+      const test = await octokit.request("GET /repos/{owner}/{repo}/commits", {
+        owner: req.params.owner,
+        repo: req.params.repo,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      });
+      res.status(200).send({ data: test.data });
+    } catch (err) {
+      res.status(404).send("INVALID DETAILS");
+    }
+  });
+
+  const port = configs.PORT;
+  app.listen(port, () => {
+    console.log("Server is running on port 3000");
+  });
+})();
