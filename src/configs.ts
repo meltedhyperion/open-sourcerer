7f638e2aaf36b26126108f2cc1580e14d85a7e6b
Kevin-Aaaquil <eshansingh2409@gmail.com>
2023-04-14 13:08:39 +0530

    basic server

---
+++ src/configs.ts
@@ -0,0 +1,9 @@
+//import dotenv and export all env variables from here
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+    PORT: process.env.PORT || 3000,
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce',
+    REDIS_URI: process.env.REDIS_URI || 'redis://localhost:6379',
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import express from 'express';
+import configs from './configs';
+
+
+(async ()=>{
+    const app = express();
+    
+    app.get('/', (req, res)=>{
+        res.send('Hello World');
+    })
+
+    const port = configs.PORT
+    app.listen(port, ()=>{
+        console.log('Server is running on port 3000');
+    })
+})()
